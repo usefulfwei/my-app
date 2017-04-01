bb659825a8afc8642c90df1a1f78be319b9d2b2f
usefulfwei <lichang.zhang@aalto.fi>
2017-04-01 19:04:19 +0300

    refine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import * as data from '../data.json';
 import './App.css';
 
 const data = [
@@ -7,117 +6,118 @@ const data = [
     "id":1,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":2,
     "name":"John Doe",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":3,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":4,
     "name":"hehe",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":5,
     "name":"Messi",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":7,
     "name":"CR",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":8,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":9,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":10,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":11,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":12,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":13,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":14,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":15,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":16,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":17,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":18,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":19,
     "name":"lichang zhang",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   },
   {
     "id":20,
     "name":"Cristiano",
     "email":"zhanglichang2016@gmail.com",
-    "phone":1234567890
+    "phone":0o417517534
   }
 ];
+
 
 class App extends Component {
   constructor(props){
@@ -133,12 +133,21 @@ class App extends Component {
     let email = this.refs.email.value.trim();
     let lists = this.state.lists;
     let length = this.state.length;
+    let emailTest = /^[^\.@]+@[^\.@]+\.[a-z]+$/;
+    let phoneTest = /^\d{5,10}$/;
     if(!name || !phone || !email){
-      alert('fill all the blank');
+      alert('please fill the blank');
+      return;
+    }
+    if(!phoneTest.test(phone)){
+      alert('please fill in a number');
+      return;
+    }
+    if(!emailTest.test(email)){
+      alert('please fill in right form of email');
       return;
     }
     let newUser;
-    // let wrongId = parseInt(new Date().getSeconds()+Math.random()*10);
     newUser = {
       id: length+2,
       name: name,
@@ -146,7 +155,7 @@ class App extends Component {
       phone: phone,
       editState: false
     };
-    lists.push(newUser);
+    lists.unshift(newUser);
     this.setState({lists:lists,length:lists.length})
     this.refs.name.value = '';
     this.refs.phone.value = '';
@@ -170,7 +179,7 @@ class App extends Component {
         let { editState } = lists[i];
         lists[i].editState = !editState;/*取反*/
         lists[i] = data;
-        return this.setState({lists});
+        break;
       }
     }
     this.setState({lists})
@@ -195,7 +204,7 @@ class App extends Component {
             <div className="col-md-3"><input type="phone" ref="phone" placeholder="Phone number"/></div>
             <button className="btn col-md-2" onClick={this.addNew.bind(this)}>Add new</button>
         </div>
-        <div className=" row header">
+        <div className="row header">
           <div className="col-md-3"><p>Name</p></div>
           <div className="col-md-4"><p>E-mail address</p></div>
           <div className="col-md-3" ><p>Phone number</p></div>
@@ -212,14 +221,62 @@ class App extends Component {
     );
   }
 }
+/*
+ handleChangeVal(e,key){
+ let val = e.target.value;
+ if(key=='title'){
+ this.setState({title:val})
+ }else{
+ this.setState({content:val})
+ }
+ }
 
+ onChange={(e)=>{this.handleChangeVal(e,'content')}} value={this.state.content}
+* */
 class List extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      name:'',
+      phone:'',
+      email:'',
+    }
+  }
+  componentDidMount(){
+    this.setState({
+      name:this.props.list.name,
+      phone:this.props.list.phone,
+      email:this.props.list.email,
+    })
+  }
+  onChange(e,key){
+    let text = e.target.value;
+    if(key === 'name'){
+      this.setState({name:text})
+    }
+    if(key === 'phone'){
+      this.setState({phone:text})
+    }
+    if(key === 'email'){
+      this.setState({email:text})
+    }
+  }
   saveItem(){
     let name = this.refs.name.value.trim();
     let phone = this.refs.phone.value.trim();
     let email = this.refs.email.value.trim();
+    let emailTest = /^[^\.@]+@[^\.@]+\.[a-z]+$/;
+    let phoneTest = /^\d{5,10}$/;
     if(!name || !phone || !email){
       alert('please fill the blank');
+      return;
+    }
+    if(!phoneTest.test(phone)){
+      alert('please fill in a number');
+      return;
+    }
+    if(!emailTest.test(email)){
+      alert('please fill in right form of email');
       return;
     }
     this.props.saveItem({
@@ -233,9 +290,9 @@ class List extends Component{
     let {id,name,phone,email,editState = false} = this.props.list;
     let showPart = editState ? (
         <div className="row lists">
-            <div className="col-md-3"><input  type="text" ref="name" placeholder="Full name"/></div>
-            <div className="col-md-3"><input  type="email" ref="email" placeholder="E-mail address"/></div>
-            <div className="col-md-3"><input type="phone" ref="phone" placeholder="Phone number"/></div>
+            <div className="col-md-3"><input  type="text" ref="name" onChange={(e) => this.onChange(e,'name')} value={this.state.name}/></div>
+            <div className="col-md-3"><input  type="email" ref="email" onChange={(e) => this.onChange(e,'email')} value={this.state.email}/></div>
+            <div className="col-md-3"><input type="phone" ref="phone" onChange={(e) => this.onChange(e,'phone')} value={this.state.phone}/></div>
             <div className="col-md-3">
               <button className="btn  save" onClick={this.saveItem.bind(this)}>Save</button>
               <button className="btn  cancel" onClick={()=> this.props.delItem(id)}>Delete</button>
